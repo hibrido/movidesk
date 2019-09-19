3e5cc2a629c11f00dd9f6b635e4e82b9dca1139c
Misso Marchewsky <missoxd@gmail.com>
2019-09-19 18:44:18 -0300

    Create movidesk.js

---
+++ movidesk.js
@@ -0,0 +1,7 @@
+setInterval(function () {
+    jQuery('.subject:contains(#1)').parents('.field-item').css('background', '#f3ffe3');
+    jQuery('.subject:contains(#2)').parents('.field-item').css('background', '#ffffd1');
+    jQuery('.subject:contains(#3)').parents('.field-item').css('background', '#c4faf8');
+    jQuery('.subject:contains(#4)').parents('.field-item').css('background', '#fbe4ff');
+    jQuery('.subject:contains(#4)').parents('.field-item').css('background', '#ecd4ff');
+}, 1000);
