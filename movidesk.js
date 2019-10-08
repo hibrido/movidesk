e3a7e3d170ede8c7ef4e509ecf3a4e2fea789e40
Misso Marchewsky <missoxd@gmail.com>
2019-10-08 19:26:05 -0300

    Update movidesk.js

--- movidesk.js
+++ movidesk.js
@@ -40,7 +40,7 @@ jQuery('body').on('click', '.play-time', function () {
 
         input.val(worked_minutes.toString().padStart(2, '0') +':'+ worked_seconds.toString().padStart(2, '0'));
         input.trigger('change');
-    }, 1000);
+    }, 60000);
 });
 
 jQuery('body').on('click', '.pause-time', function () {
