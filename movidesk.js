322b0f3ae5b2e489baa5c7de3fe39bcf61dcb581
Misso Marchewsky <missoxd@gmail.com>
2019-10-08 19:24:09 -0300

    Update movidesk.js

--- movidesk.js
+++ movidesk.js
@@ -12,38 +12,38 @@ setInterval(function () {
         }
         
         if (jQuery(this).find('.ticket-appointments-container .play-pause-content').length == 0) {
-            jQuery(this).find('.ticket-appointments-container').prepend('<div class="ticket-appointments-little-container play-pause-content"><div class="mv-input-container time-appointment-date"><button type="button" class="play-time" data-toggle="popover" data-trigger="focus" data-content="Apontamentos" data-original-title="">&gt;</button><button type="button" class="pause-time" data-toggle="popover" data-trigger="focus" data-content="Apontamentos" data-original-title="" title="">||</button></div></div>');
+            jQuery(this).find('.ticket-appointments-container').prepend('<div class="ticket-appointments-little-container play-pause-content"><div class="mv-input-container time-appointment-date"><button type="button" class="play-time" style="margin-right:10px;padding-top:5px;"><span class="icon-mv-play"></span></button><button type="button" class="pause-time" style="padding-top:5px;"><span class="icon-mv-pause"></span></button></div></div>');
         }
     });
     
 }, 1000);
 
 jQuery('body').on('click', '.play-time', function () {
-	let input = jQuery(this).parents('.ticket-appointments').find('.time-appointment-time input');
-	window.plays = window.plays || {};
-	window.plays[input.attr('id')] = setInterval(function () {
-		if (input.val() == '') {
-			input.val('00:00');
-			return;
-		}
-
-		let worked_split = input.val().split(':');
-		let worked_minutes = worked_split[0];
-		let worked_seconds = worked_split[1];
-
-		worked_seconds++;
-
-		if (worked_seconds >= 60) {
-			worked_minutes++;
-			worked_seconds = 0;
+    let input = jQuery(this).parents('.ticket-appointments').find('.time-appointment-time input');
+    window.plays = window.plays || {};
+    window.plays[input.attr('id')] = setInterval(function () {
+        if (input.val() == '') {
+            input.val('00:00');
+            return;
         }
 
-		input.val(worked_minutes.toString().padStart(2, '0') +':'+ worked_seconds.toString().padStart(2, '0'));
-		input.trigger('change');
-	}, 1000);
+        let worked_split = input.val().split(':');
+        let worked_minutes = worked_split[0];
+        let worked_seconds = worked_split[1];
+
+        worked_seconds++;
+        
+        if (worked_seconds >= 60) {
+            worked_minutes++;
+            worked_seconds = 0;
+        }
+
+        input.val(worked_minutes.toString().padStart(2, '0') +':'+ worked_seconds.toString().padStart(2, '0'));
+        input.trigger('change');
+    }, 1000);
 });
 
 jQuery('body').on('click', '.pause-time', function () {
-	let input = jQuery(this).parents('.ticket-appointments').find('.time-appointment-time input');
-	clearInterval(window.plays[input.attr('id')]);
+    let input = jQuery(this).parents('.ticket-appointments').find('.time-appointment-time input');
+    clearInterval(window.plays[input.attr('id')]);
 });
